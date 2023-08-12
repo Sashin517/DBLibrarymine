6b8b118816a4ae96cce875d2eb6558127faf8518
Sashin517 <114796882+Sashin517@users.noreply.github.com>
2023-08-12 14:21:22 +0530

    Update connection1.php

--- connection1.php
+++ connection1.php
@@ -11,21 +11,27 @@ if (isset($_POST['email'])) {
         die("Connection to the database failed due to" . mysqli_connect_error());
     }
 
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $dob = $_POST['dob'];
     $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $uname = $_POST['uname'];
     $password = $_POST['password'];
 
-    $select = "SELECT * FROM `member` WHERE email = '$email' AND password = '$password' ";
+    $select = "SELECT * FROM `member` WHERE email = '$email'";
 
     $result = mysqli_query($con, $select);
 
     if (mysqli_num_rows($result) > 0) {
         echo "User already exists!";
     } else {
-        $sql = "INSERT INTO `member` (`email`, `password`) VALUES ('$email', '$password');";
+        $sql = "INSERT INTO `member` (`first_name`, `last_name`, `DOB`, `email`, `phone_no`, `user_name`, `password`) 
+                VALUES ('$fname', '$lname', '$dob', '$email', '$phone', '$uname', '$password');";
 
         if ($con->query($sql) === true) {
             echo "You're Successfully Registered";
-            header('Location: login.php');
+            header('Location: login.html'); // Redirect to login page after successful registration
         } else {
             echo "Error: $sql <br>" . $con->error;
         }
